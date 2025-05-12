6e94a607626e3d2b9bdf43a4b02c6ba0c4c41338
Craig McKeachie <craigdmckeachie@gmail.com>
2025-05-12 13:38:10 -0400

    dealership file manager empty methods

--- CarDealership/src/main/java/com/pluralsight/DealershipFileManager.java
+++ CarDealership/src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,7 +1,15 @@
 package com.pluralsight;
 
 public class DealershipFileManager {
-    public Dealership getDealership() {
+
+    public Dealership getDealership(  ) {
         Dealership dealership = new Dealership("","", "");
+        return dealership;
     }
+
+
+    public void saveDealership(Dealership dealership){
+
+    }
+
 }
