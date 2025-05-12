ebec5c7318279a011f203e19a7d27aaf91cb3d45
Craig McKeachie <craigdmckeachie@gmail.com>
2025-05-12 14:28:38 -0400

    add user interface and program

--- CarDealership/src/main/java/com/pluralsight/DealershipFileManager.java
+++ CarDealership/src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,8 +1,35 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class DealershipFileManager {
 
     public Dealership getDealership(  ) {
+        //read inventory.csv file
+        //read the first line which has the dealership data
+        //take the line of dealership data and split by | which returns a string array of each piece of dealership data
+        //take each column/piece of data and pass it into the constructor of our Dealership to create a specific dealership object
+
+        try {
+            FileReader fileReader = new FileReader("data/inventory.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file.");
+            e.printStackTrace();
+        }
+
+
+
         Dealership dealership = new Dealership("","", "");
         return dealership;
     }
